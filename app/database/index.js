439ddf94efa3d121a63ee3d6f6f81160e28f3c17
gaveer <45834601+gaveer@users.noreply.github.com>
2021-03-17 22:35:41 +0300

    first commit

---
+++ app/database/index.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var config = require("../config");
+var Mongoose = require("mongoose");
+var logger = require("../logger");
+
+// Connect to the database
+// construct the database URI and encode username and password.
+var dbURI =
+  "mongodb+srv://test:XEQSzIqyHF84YKYM@data-db.ivchh.mongodb.net/Test?retryWrites=true&w=majority";
+Mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+// Throw an error if the connection fails
+Mongoose.connection.on("error", function (err) {
+  if (err) throw err;
+});
+
+// mpromise (mongoose's default promise library) is deprecated,
+// Plug-in your own promise library instead.
+// Use native promises
+Mongoose.Promise = global.Promise;
+
+module.exports = {
+  Mongoose,
+  models: {
+    user: require("./schemas/user.js"),
+    room: require("./schemas/room.js"),
+  },
+};
